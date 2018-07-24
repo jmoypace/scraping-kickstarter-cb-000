14e860c77a452ce3bbcf0e9157f097d9e16de19d
jmoypace <jmoypace@gmail.com>
2018-07-24 23:43:19 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,8 +5,9 @@ require "nokogiri"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-
+binding.pry
 end
 
+
+
 create_project_hash
-binding.pry
